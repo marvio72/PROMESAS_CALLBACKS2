9697f3bc352f45eea37124ef46e7a5af99deaa49
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-08-31 19:27:36 -0500

    Promise.all

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { buscarHeroe as buscarHeroeCallback } from './js/callbacks';
 import {buscarHeroe} from './js/promesas';
 
 const heroeId1 = 'capi';
-// const heroeId2 = 'iron';
+const heroeId2 = 'iron';
 
 //  buscarHeroe( heroeId1, ( err, heroe1 ) => {
 
@@ -18,10 +18,17 @@ const heroeId1 = 'capi';
 
 //  });
 
-buscarHeroe( heroeId1 ).then( heroe => {
+// buscarHeroe( heroeId1 ).then( heroe1 => { 
+//   // console.log(`Enviando a ${heroe.nombre} a la misión`);
+//   buscarHeroe(heroeId2).then( heroe2 => {
+//     console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la misión`);
+//   });
+// });
 
-  console.log(`Enviando a ${heroe.nombre} a la misión`);
+Promise.all([buscarHeroe(heroeId1), buscarHeroe(heroeId2)])
+  .then(([heroe1,heroe2]) => {
 
+  console.log(`Enviando a ${heroe1.nombre}  y ${heroe2.nombre} a la misión`);
 });
 
  console.log('Fin del programa');
